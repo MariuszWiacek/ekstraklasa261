fd3ae6e4502bd76ebc7d250379e46e73997db018
MariuszLillup <Mariusz@lillup.com>
2026-01-27 09:33:56 +0000

    firebase

--- src/pages/bets.js
+++ src/pages/bets.js
@@ -16,6 +16,7 @@ import { DateTime } from 'luxon';
 const firebaseConfig = {
   apiKey: "AIzaSyB3AOrOzAQ-WVMjeZ3ayNwklR7axBgXJ0I",
   authDomain: "wiosna26-951d6.firebaseapp.com",
+  databaseURL: "https://wiosna26-951d6-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "wiosna26-951d6",
   storageBucket: "wiosna26-951d6.firebasestorage.app",
   messagingSenderId: "58145083288",
