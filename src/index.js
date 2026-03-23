2985254178454850668d87c6762956002d214e2e
Mariusz Wiacek <117862308+MariuszWiacek@users.noreply.github.com>
2026-03-23 20:58:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
+// 👇 ADD THIS
+import { register } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+<App />
+</React.StrictMode>
 );
+
+// 👇 ADD THIS
+register();
 
 reportWebVitals();
