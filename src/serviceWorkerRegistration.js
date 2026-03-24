2b07ed348095c55dd6b142d59e565ac8d75b5d31
Mariusz Wiacek <117862308+MariuszWiacek@users.noreply.github.com>
2026-03-24 02:13:06 +0000

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,10 +1,16 @@
 export function register() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((reg) => console.log('SW Registered!', reg))
-        .catch((err) => console.log('SW Registration failed:', err));
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
+      try {
+        const reg = await navigator.serviceWorker.register("/service-worker.js");
+        console.log("✅ Service Worker Registered", reg);
+
+        navigator.serviceWorker.ready.then(() => {
+          console.log("✅ Service Worker Ready");
+        });
+      } catch (err) {
+        console.log("❌ SW registration failed:", err);
+      }
     });
   }
 }
