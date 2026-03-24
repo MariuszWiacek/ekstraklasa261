ed8a4dec3e78b90b6b9afa99a580b1428783ae28
MariuszLillup <Mariusz@lillup.com>
2026-03-24 07:57:41 +0000

    hh

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,13 @@
-const CACHE_NAME = "ekstrabet-v1";
-
-self.addEventListener("install", (event) => {
-  console.log("SW installed");
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icon-192.png",
-        "/icon-512.png"
-      ]);
-    })
-  );
-
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("SW activated");
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // no caching yet
 });
